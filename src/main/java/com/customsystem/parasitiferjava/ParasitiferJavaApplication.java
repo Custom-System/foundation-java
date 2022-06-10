4b07f72e17c4b3d1979454e53e8c507a8dcb3fe0
刘龙 <kercylan@gmail.com>
2022-06-10 14:54:57 +0800

    init project

---
+++ src/main/java/com/customsystem/parasitiferjava/ParasitiferJavaApplication.java
@@ -0,0 +1,13 @@
+package com.customsystem.parasitiferjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParasitiferJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParasitiferJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/customsystem/parasitiferjava/ParasitiferJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customsystem.parasitiferjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParasitiferJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
